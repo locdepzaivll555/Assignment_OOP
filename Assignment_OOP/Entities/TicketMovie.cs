0d12e0aea7ac1ab1486d6f60686ad7cc822b1d05
locngvan357@outlook.com.vn <locngvan357@outlook.com.vn>
2025-03-15 08:58:32 +0700

    Add project files.

---
+++ Assignment_OOP/Entities/TicketMovie.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment_OOP.Entities
+{
+    public class Account
+    {
+        public string Username { get; set; }
+        public string Password { get; set; }
+
+        public string Email { get; set; }
+
+        public string Phone{ get; set; }
+
+        public bool Status { get; set; }/// false: user, true: admin
+    }
+    public class TicketMovie
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string MovieName { get; set; }
+        public string Room { get; set; }
+        public string[,] Seat { get; set; }
+        public DateTime Time { get; set; }
+        public double Price { get; set; }
+        public bool Status { get; set; }
+    }
+    public class Movie
+    {
+        public string NameMovie { get; set; }
+        public string Director { get; set; }
+        public string Genre { get; set; }
+        public string Language { get; set; }
+        public string Subtitle { get; set; }
+        public int Duration { get; set; }
+        public DateTime ReleaseDate { get; set; }
+        public bool Status { get; set; }
+    }
+}
---
+++ Assignment_OOP/Implements/MovieApp.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment_OOP.Implements
+{
+    public abstract class MovieApp <Model>
+    {
+        public abstract void MainMenu();
+        public abstract Model UpdateMovie();
+        public abstract void ShowMovies();
+        public abstract void ChoseMovie();
+        public abstract void ShowTime();
+        public abstract void ChoseTime();
+        public abstract void ShowSeat();
+        public abstract void ChoseSeat();
+        public abstract void ShowPrice();
+        public abstract void Payment();
+        public abstract void ShowTicket();
+
+    }
+}
